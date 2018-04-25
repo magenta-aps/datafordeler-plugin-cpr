ab47c0cbb6e88a2b92cd3b0e9e2b980f16f83517
Lars Peter Thomsen <larsp@magenta.dk>
2018-04-25 12:45:53 +0200

    Debugging

--- src/main/java/dk/magenta/datafordeler/cpr/data/person/PersonRegistration.java
+++ src/main/java/dk/magenta/datafordeler/cpr/data/person/PersonRegistration.java
@@ -1,5 +1,6 @@
 package dk.magenta.datafordeler.cpr.data.person;
 
+import dk.magenta.datafordeler.cpr.CprPlugin;
 import dk.magenta.datafordeler.cpr.data.CprRegistration;
 
 import javax.persistence.Index;
@@ -11,10 +12,10 @@ import java.time.OffsetDateTime;
  * @see dk.magenta.datafordeler.core.database.Entity
  */
 @javax.persistence.Entity
-@Table(name="cpr_person_registration", indexes = {
-        @Index(name = "cpr_person_entity", columnList = "entity_id"),
-        @Index(name = "cpr_person_registration_from", columnList = "registrationFrom"),
-        @Index(name = "cpr_person_registration_to", columnList = "registrationTo")
+@Table(name= CprPlugin.DEBUG_TABLE_PREFIX + "cpr_person_registration", indexes = {
+        @Index(name = CprPlugin.DEBUG_TABLE_PREFIX + "cpr_person_entity", columnList = "entity_id"),
+        @Index(name = CprPlugin.DEBUG_TABLE_PREFIX + "cpr_person_registration_from", columnList = "registrationFrom"),
+        @Index(name = CprPlugin.DEBUG_TABLE_PREFIX + "cpr_person_registration_to", columnList = "registrationTo")
 })
 public class PersonRegistration extends CprRegistration<PersonEntity, PersonRegistration, PersonEffect> {
 
