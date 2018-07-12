d12f00bf2556447d5b0eced2c1b46ed25e66daed
Lars Peter Thomsen <larsp@magenta.dk>
2018-07-12 14:33:21 +0200

    New service to handle alternate approach to data storage

---
+++ src/main/java/dk/magenta/datafordeler/cpr/records/service/PersonEntityRecordService.java
@@ -0,0 +1,103 @@
+package dk.magenta.datafordeler.cpr.records.service;
+
+import dk.magenta.datafordeler.core.MonitorService;
+import dk.magenta.datafordeler.core.arearestriction.AreaRestriction;
+import dk.magenta.datafordeler.core.arearestriction.AreaRestrictionType;
+import dk.magenta.datafordeler.core.database.DataItem;
+import dk.magenta.datafordeler.core.exception.AccessDeniedException;
+import dk.magenta.datafordeler.core.exception.AccessRequiredException;
+import dk.magenta.datafordeler.core.exception.InvalidClientInputException;
+import dk.magenta.datafordeler.core.fapi.FapiService;
+import dk.magenta.datafordeler.core.fapi.OutputWrapper;
+import dk.magenta.datafordeler.core.plugin.AreaRestrictionDefinition;
+import dk.magenta.datafordeler.core.plugin.Plugin;
+import dk.magenta.datafordeler.core.user.DafoUserDetails;
+import dk.magenta.datafordeler.cpr.CprAccessChecker;
+import dk.magenta.datafordeler.cpr.CprAreaRestrictionDefinition;
+import dk.magenta.datafordeler.cpr.CprPlugin;
+import dk.magenta.datafordeler.cpr.CprRolesDefinition;
+import dk.magenta.datafordeler.cpr.data.person.PersonEntity;
+import dk.magenta.datafordeler.cpr.data.person.PersonOutputWrapper;
+import dk.magenta.datafordeler.cpr.data.person.PersonQuery;
+import dk.magenta.datafordeler.cpr.data.person.data.PersonBaseData;
+import dk.magenta.datafordeler.cpr.records.output.PersonRecordOutputWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/cpr/person/2/rest")
+public class PersonEntityRecordService extends FapiService<PersonEntity, PersonQuery> {
+
+    @Autowired
+    private CprPlugin cprPlugin;
+
+    @Autowired
+    private MonitorService monitorService;
+
+    @Autowired
+    private PersonRecordOutputWrapper personRecordOutputWrapper;
+
+    @PostConstruct
+    public void init() {
+        this.monitorService.addAccessCheckPoint("/cpr/person/2/rest/1234");
+        this.monitorService.addAccessCheckPoint("/cpr/person/2/rest/search?personnummer=1234");
+        this.setOutputWrapper(this.personRecordOutputWrapper);
+    }
+
+    @Override
+    protected OutputWrapper.Mode getDefaultMode() {
+        return OutputWrapper.Mode.LEGACY;
+    }
+
+    @Override
+    public int getVersion() {
+        return 1;
+    }
+
+    @Override
+    public String getServiceName() {
+        return "person";
+    }
+
+    @Override
+    protected Class<PersonEntity> getEntityClass() {
+        return PersonEntity.class;
+    }
+
+    @Override
+    protected Class<? extends DataItem> getDataClass() {
+        return PersonBaseData.class;
+    }
+
+    @Override
+    public Plugin getPlugin() {
+        return this.cprPlugin;
+    }
+
+    @Override
+    protected void checkAccess(DafoUserDetails dafoUserDetails) throws AccessDeniedException, AccessRequiredException {
+        CprAccessChecker.checkAccess(dafoUserDetails);
+    }
+
+    @Override
+    protected PersonQuery getEmptyQuery() {
+        return new PersonQuery();
+    }
+
+    @Override
+    protected void applyAreaRestrictionsToQuery(PersonQuery query, DafoUserDetails user) throws InvalidClientInputException {
+        Collection<AreaRestriction> restrictions = user.getAreaRestrictionsForRole(CprRolesDefinition.READ_CPR_ROLE);
+        AreaRestrictionDefinition areaRestrictionDefinition = this.cprPlugin.getAreaRestrictionDefinition();
+        AreaRestrictionType municipalityType = areaRestrictionDefinition.getAreaRestrictionTypeByName(CprAreaRestrictionDefinition.RESTRICTIONTYPE_KOMMUNEKODER);
+        for (AreaRestriction restriction : restrictions) {
+            if (restriction.getType() == municipalityType) {
+                query.addKommunekodeRestriction(restriction.getValue());
+            }
+        }
+    }
+
+}
