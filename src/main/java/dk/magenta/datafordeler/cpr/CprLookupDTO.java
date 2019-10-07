ed58471958c800ca9fa7b49fe5f2fd26d4a5ed01
madsmj <madsmj@gmail.com>
2019-10-07 09:31:39 +0200

    moving cpr-related lookup from geo to cpr

---
+++ src/main/java/dk/magenta/datafordeler/cpr/CprLookupDTO.java
@@ -0,0 +1,67 @@
+package dk.magenta.datafordeler.cpr;
+
+public class CprLookupDTO {
+    protected String municipalityName = null;
+    protected String roadName = null;
+    protected String localityCode = null;
+    protected String localityAbbrev = null;
+    protected String localityName = null;
+    protected int postalCode = 0;
+    protected String postalDistrict = null;
+
+    public String getMunicipalityName() {
+        return municipalityName;
+    }
+
+    public void setMunicipalityName(String municipalityName) {
+        this.municipalityName = municipalityName;
+    }
+
+    public String getRoadName() {
+        return roadName;
+    }
+
+    public void setRoadName(String roadName) {
+        this.roadName = roadName;
+    }
+
+    public String getLocalityCode() {
+        return localityCode;
+    }
+
+    public void setLocalityCode(String localityCode) {
+        this.localityCode = localityCode;
+    }
+
+    public String getLocalityAbbrev() {
+        return localityAbbrev;
+    }
+
+    public void setLocalityAbbrev(String localityAbbrev) {
+        this.localityAbbrev = localityAbbrev;
+    }
+
+    public String getLocalityName() {
+        return localityName;
+    }
+
+    public void setLocalityName(String localityName) {
+        this.localityName = localityName;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPostalDistrict() {
+        return postalDistrict;
+    }
+
+    public void setPostalDistrict(String postalDistrict) {
+        this.postalDistrict = postalDistrict;
+    }
+}
